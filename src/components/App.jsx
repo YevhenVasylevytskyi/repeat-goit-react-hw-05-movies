d28e391de0bdb0a14b787b4cf8b826930503fc03
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-06-04 13:03:29 +0300

    add SearchMovie

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,27 +5,33 @@ import MovieDetails from "../pages/MovieDetails";
 import Cast from "../components/Cast";
 import Reviews from "../components/Reviews";
 import Container from "./Container/Container";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import s from "./App.module.css"
 
 
 export const App = () => {
   return (
-    <Container>
-      <nav className={s.nav}>
-        <NavLink className={s.navLink} to="/">Home</NavLink>
-        <NavLink className={s.navLink} to="/movies">Movies</NavLink>
-      </nav>
-      <div>
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/movies" element={ <Movies /> } />
-          <Route path="/movies/:movieId" element={<MovieDetails />} >  
-          <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-        </Routes>
-      </div>
-    </Container>
+    <>
+      <Container>
+        <nav className={s.nav}>
+          <NavLink className={s.navLink} to="/">Home</NavLink>
+          <NavLink className={s.navLink} to="/movies">Movies</NavLink>
+        </nav>
+        <div>
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/movies" element={ <Movies /> } />
+            <Route path="/movies/:movieId" element={<MovieDetails />} >  
+            <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Routes>
+        </div>
+      </Container>
+      <ToastContainer />
+    </>
+    
   );
 };
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,11 +1,72 @@
-// import s from './Movies.module.css';
+import { useState, useEffect } from "react";
+import { FiSearch } from "react-icons/fi";
+import { toast } from 'react-toastify';
+import { fetchSearchMovies } from "services/MovieService";
+
+import s from "./Movies.module.css";
 
 function Movies() {
+
+    const [searchMovie, setSearchMovie] = useState('');
+    const [movies, setMovies] = useState(null);
+
+    useEffect(() => {
+        fetchSearchMovies(searchMovie)
+            .then(response => {
+                setMovies(response.results)
+            })        
+    }, [searchMovie])
+
+    // console.log(movies)
+
+    const handleChange = event => {
+        setSearchMovie(event.currentTarget.value.toLowerCase())
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        if (searchMovie.trim() === "") {
+            return toast.error("Введіть пошуковий запит");
+        }
+        // onSubmit(searchMovie);
+        setSearchMovie('');
+    }
+    
     return (
         <>
-            <p>Movies</p>
+            <form
+                className={s.SearchForm}
+              onSubmit={handleSubmit}
+            >
+                <button
+                    type="submit"
+                    className={s.SearchFormButton}
+                >
+                    <FiSearch className={s.SearchSvg} />
+                </button>
+
+                <input
+                    className={s.SearchFormInput}
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search movie"
+                    value={searchMovie}
+                    onChange={handleChange}
+                />
+            </form>
+            <ul>
+                {movies && movies.map(movie => {
+                    return <li key={movie.id}>
+                        {movie.title}
+                    </li>
+                })}                
+            </ul>
         </>
-    );
-}
+
+        );
+    }
+
 
 export default Movies;
